2af12485e70e6142e970cbda1e6ff46ae114f8e5
Marcus <marcp20@student.sdu.dk>
2021-04-13 14:04:00 +0200

    change DBController into proxy/facade for database wrapper implementation

--- com/company/DBController.java
+++ com/company/DBController.java
@@ -2,8 +2,9 @@ package com.company;
 
 import com.company.data.Database;
 import com.company.data.JsonDatabase;
+import com.company.data.Production;
 
-public class DBController {
+public class DBController implements Database{
     private Database database;
     private DBController instance;
 
@@ -12,6 +13,34 @@ public class DBController {
     }
 
     public DBController getInstance() {
+        if(this.instance == null) {
+            this.instance = new DBController();
+        }
         return this.instance;
+    }
+
+    @Override
+    public void getProductions() {
+
+    }
+
+    @Override
+    public void addProduction(Production production) {
+
+    }
+
+    @Override
+    public void getCredits(Production production) {
+
+    }
+
+    @Override
+    public void addCredit(Production production) {
+
+    }
+
+    @Override
+    public boolean validLogin(String username, String password) {
+        return false;
     }
 }
