0e0b54707cd1fc5e21265f36d390924a5ffa25e0
Marcus <marcp20@student.sdu.dk>
2021-04-13 13:49:46 +0200

    make DBController singleton

--- com/company/DBController.java
+++ com/company/DBController.java
@@ -5,8 +5,13 @@ import com.company.data.JsonDatabase;
 
 public class DBController {
     private Database database;
+    private DBController instance;
 
-    public DBController() {
+    private DBController() {
         database = new JsonDatabase();
+    }
+
+    public DBController getInstance() {
+        return this.instance;
     }
 }
