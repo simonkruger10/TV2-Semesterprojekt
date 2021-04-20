5a8aba7e5305cff4dca76aec69e9117840137fbe
Henrik Ankersø <heank16@student.sdu.dk>
2021-04-20 15:02:40 +0200

    Update Credit

--- src/main/java/com/company/domain/CreditManagement.java
+++ src/main/java/com/company/domain/CreditManagement.java
@@ -1,4 +1,33 @@
 package com.company.domain;
 
 public class CreditManagement implements ICreditManagement {
+    @Override
+    public CreditDTO createCredit(String firstName) {
+        return null;
+    }
+
+    @Override
+    public CreditDTO createCredit(String firstName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public CreditDTO createCredit(String firstName, String middleName, String lastName) {
+        return null;
+    }
+
+    @Override
+    public CreditDTO[] getCreditByName(String name) {
+        return new CreditDTO[0];
+    }
+
+    @Override
+    public CreditDTO[] getCreditByName(String firstName, String lastName) {
+        return new CreditDTO[0];
+    }
+
+    @Override
+    public CreditDTO[] getCreditByName(String firstName, String middleName, String lastName) {
+        return new CreditDTO[0];
+    }
 }
--- src/main/java/com/company/domain/ICreditManagement.java
+++ src/main/java/com/company/domain/ICreditManagement.java
@@ -1,4 +1,11 @@
 package com.company.domain;
 
 public interface ICreditManagement {
+    CreditDTO createCredit(String firstName);
+    CreditDTO createCredit(String firstName, String lastName);
+    CreditDTO createCredit(String firstName, String middleName, String lastName);
+
+    CreditDTO[] getCreditByName(String name);
+    CreditDTO[] getCreditByName(String firstName, String lastName);
+    CreditDTO[] getCreditByName(String firstName, String middleName, String lastName);
 }
