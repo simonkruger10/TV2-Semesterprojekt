c589a73d0b32b8ceb4f6cab9b699d9dc24ed7d32
Henrik Ankersø <heank16@student.sdu.dk>
2021-04-20 12:23:43 +0200

    Convert to maven and fix JavaFX setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.company {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires org.json;
+    opens com.company.gui to javafx.graphics, javafx.fxml;
+    opens com.company;
+}
