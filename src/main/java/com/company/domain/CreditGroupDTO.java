6b53ac8f4b616552f9c8eca96c97a0d751836db1
Henrik Ankersø <heank16@student.sdu.dk>
2021-04-22 19:03:20 +0200

    Update credit group

--- src/main/java/com/company/domain/CreditGroupDTO.java
+++ src/main/java/com/company/domain/CreditGroupDTO.java
@@ -2,18 +2,29 @@ package com.company.domain;
 
 import com.company.common.ICreditGroup;
 
+import java.util.UUID;
+
 public class CreditGroupDTO implements ICreditGroup {
+    private String name = null;
+    private String uuid = UUID.randomUUID().toString();
+
     @Override
     public String getUUID() {
-        return null;
+        return uuid;
     }
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
+    }
 
+    public void copyCreditGroup(ICreditGroup creditGroup) {
+        assert creditGroup != null;
+
+        this.setName(creditGroup.getName());
     }
 }
