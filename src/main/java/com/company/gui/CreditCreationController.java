39eaa42591deef4ce0b88eebe5e591406166ca59
Oliver <oliver.hinsch@gmail.com>
2021-04-25 15:28:01 +0200

    Minor update

--- src/main/java/com/company/gui/CreditCreationController.java
+++ src/main/java/com/company/gui/CreditCreationController.java
@@ -1,13 +1,14 @@
 package com.company.gui;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class CreditCreationController extends GuiController {
+public class CreditCreationController extends GuiController{
 
     @FXML
     private Text logOutBtn;
@@ -19,17 +20,41 @@ public class CreditCreationController extends GuiController {
     private TextField searchBarField;
 
     @FXML
+    private TextField firstNameText;
+
+    @FXML
+    private TextField middleNameText;
+
+    @FXML
+    private TextField lastNameText;
+
+    @FXML
+    private TextField creditGroupText;
+
+    @FXML
+    private Button addCreditBtn;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    void addCreditBtn(MouseEvent event) {
+
+    }
+
+    @FXML
     void changePassword(MouseEvent event) {
 
     }
 
     @FXML
     void logOut(MouseEvent event) {
-        Platform.exit();
+
     }
 
     @FXML
     void searchProduction(KeyEvent event) {
 
     }
+
 }
