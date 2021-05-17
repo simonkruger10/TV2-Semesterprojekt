aebecc178e4dd954c521d7f5c36ac9f3b5867690
Henrik Ankersø <heank16@student.sdu.dk>
2021-05-17 13:34:37 +0200

    Add create database

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,91 +1,49 @@
 -- noinspection SpellCheckingInspectionForFile
 
-/*
-drop database IF EXISTS tv2_semesterprojekt;
-CREATE DATABASE tv2_semesterprojekt
+--
+-- DATABASE_NAME: tv2_semesterprojekt_f3f70b5a // F3F70B5A is an Adler-32 value of tv2_semesterprojekt_17-05-2021-12:20
+-- USER_NAME: tv2_dbuser_f3f70b5a // F3F70B5A is an Adler-32 value of tv2_semesterprojekt_17-05-2021-12:20
+-- PASSWORD: 4A03069D // 4A03069D is an Adler-32 value of "asfwe 47igfsvw3443e2" without quotation marks
+--
+
+
+--
+-- Remove the tv2_semesterprojekt_f3f70b5a database and tv2_dbuser_f3f70b5a user
+--
+
+-- Kick all activity connections to the tv2_semesterprojekt_f3f70b5a database if any
+SELECT PG_TERMINATE_BACKEND(pid)
+FROM pg_stat_activity
+WHERE datname = 'tv2_semesterprojekt_f3f70b5a';
+
+-- Drop the tv2_semesterprojekt_f3f70b5a database if the database exists
+DROP DATABASE IF EXISTS tv2_semesterprojekt_f3f70b5a;
+
+-- Revoke all rights for tv2_dbuser_f3f70b5a user if the user exists
+DO
+$$BEGIN
+    IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'tv2_dbuser_f3f70b5a') THEN
+        EXECUTE 'REVOKE CONNECT ON DATABASE "postgres" FROM tv2_dbuser_f3f70b5a';
+    END IF;
+END$$;
+
+-- Drop tv2_dbuser_f3f70b5a user if the user exists
+DROP USER IF EXISTS tv2_dbuser_f3f70b5a;
+
+
+--
+-- Create tv2_dbuser_f3f70b5a database and tv2_dbuser_f3f70b5a user
+--
+
+-- Create tv2_dbuser_f3f70b5a user
+CREATE USER tv2_dbuser_f3f70b5a WITH ENCRYPTED PASSWORD '4A03069D';
+
+-- Create the tv2_semesterprojekt_f3f70b5a database
+CREATE DATABASE tv2_semesterprojekt_f3f70b5a
     WITH
-    OWNER = postgres
+    OWNER tv2_dbuser_f3f70b5a
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
- */
 
-DROP TABLE IF EXISTS account CASCADE;
-CREATE TABLE account
-(
-    id           SERIAL PRIMARY KEY,
-    f_name       VARCHAR(65)  NOT NULL,
-    m_name       VARCHAR(65),
-    l_name       VARCHAR(65),
-    email        VARCHAR(253) NOT NULL,
-    access_level INTEGER DEFAULT -1,
-    password     VARCHAR(253) NOT NULL,
-    UNIQUE (email)
-);
-
-DROP TABLE IF EXISTS producer CASCADE;
-CREATE TABLE producer
-(
-    id         SERIAL PRIMARY KEY,
-    name       VARCHAR(65) NOT NULL,
-    logo       VARCHAR(254),
-    account_id INTEGER REFERENCES account (id)
-);
-
-DROP TABLE IF EXISTS production CASCADE;
-CREATE TABLE production
-(
-    id            SERIAL,
-    name          VARCHAR(65) NOT NULL,
-    release_day   INTEGER     NOT NULL,
-    release_month INTEGER     NOT NULL,
-    release_year  INTEGER     NOT NULL,
-    description   TEXT,
-    image         VARCHAR(254),
-    producer_id   INTEGER REFERENCES producer (id),
-    PRIMARY KEY (name, release_day, release_month, release_year),
-    UNIQUE (id)
-);
-
-DROP TABLE IF EXISTS credit_unit CASCADE;
-CREATE TABLE credit_unit
-(
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(65) NOT NULL,
-    UNIQUE (name)
-);
-
-DROP TABLE IF EXISTS credit_person CASCADE;
-CREATE TABLE credit_person
-(
-    m_name VARCHAR(65),
-    l_name VARCHAR(65),
-    image  VARCHAR(254),
-    email  VARCHAR(253) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (email)
-) INHERITS (credit_unit);
-
-DROP TABLE IF EXISTS credit_group CASCADE;
-CREATE TABLE credit_group
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(65) NOT NULL,
-    description TEXT
-);
-
-DROP TABLE IF EXISTS production_credit_unit_relation CASCADE;
-CREATE TABLE production_credit_unit_relation
-(
-    production_id   INTEGER NOT NULL REFERENCES production (id),
-    credit_id       INTEGER NOT NULL REFERENCES credit_unit (id),
-    credit_group_id INTEGER NOT NULL REFERENCES credit_group (id),
-    PRIMARY KEY (production_id, credit_id, credit_group_id)
-);
-
-DROP TABLE IF EXISTS production_credit_person_relation CASCADE;
-CREATE TABLE production_credit_person_relation
-(
-    credit_id INTEGER NOT NULL REFERENCES credit_person (id),
-    PRIMARY KEY (production_id, credit_id, credit_group_id)
-) INHERITS (production_credit_unit_relation);
-
+-- Assign all rights to tv2_dbuser_f3f70b5a user
+GRANT ALL PRIVILEGES ON DATABASE tv2_semesterprojekt_f3f70b5a TO tv2_dbuser_f3f70b5a;
---
+++ src/main/resources/tables.sql
@@ -0,0 +1,81 @@
+-- noinspection SpellCheckingInspectionForFile
+
+DROP TABLE IF EXISTS account CASCADE;
+CREATE TABLE account
+(
+    id           SERIAL PRIMARY KEY,
+    f_name       VARCHAR(65)  NOT NULL,
+    m_name       VARCHAR(65),
+    l_name       VARCHAR(65),
+    email        VARCHAR(253) NOT NULL,
+    access_level INTEGER DEFAULT -1,
+    password     VARCHAR(253) NOT NULL,
+    UNIQUE (email)
+);
+
+DROP TABLE IF EXISTS producer CASCADE;
+CREATE TABLE producer
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(65) NOT NULL,
+    logo       VARCHAR(254),
+    account_id INTEGER REFERENCES account (id)
+);
+
+DROP TABLE IF EXISTS production CASCADE;
+CREATE TABLE production
+(
+    id            SERIAL,
+    name          VARCHAR(65) NOT NULL,
+    release_day   INTEGER     NOT NULL,
+    release_month INTEGER     NOT NULL,
+    release_year  INTEGER     NOT NULL,
+    description   TEXT,
+    image         VARCHAR(254),
+    producer_id   INTEGER REFERENCES producer (id),
+    PRIMARY KEY (name, release_day, release_month, release_year),
+    UNIQUE (id)
+);
+
+DROP TABLE IF EXISTS credit_unit CASCADE;
+CREATE TABLE credit_unit
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(65) NOT NULL,
+    UNIQUE (name)
+);
+
+DROP TABLE IF EXISTS credit_person CASCADE;
+CREATE TABLE credit_person
+(
+    m_name VARCHAR(65),
+    l_name VARCHAR(65),
+    image  VARCHAR(254),
+    email  VARCHAR(253) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+) INHERITS (credit_unit);
+
+DROP TABLE IF EXISTS credit_group CASCADE;
+CREATE TABLE credit_group
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(65) NOT NULL,
+    description TEXT
+);
+
+DROP TABLE IF EXISTS production_credit_unit_relation CASCADE;
+CREATE TABLE production_credit_unit_relation
+(
+    production_id   INTEGER NOT NULL REFERENCES production (id),
+    credit_id       INTEGER NOT NULL REFERENCES credit_unit (id),
+    credit_group_id INTEGER NOT NULL REFERENCES credit_group (id),
+    PRIMARY KEY (production_id, credit_id, credit_group_id)
+);
+
+DROP TABLE IF EXISTS production_credit_person_relation CASCADE;
+CREATE TABLE production_credit_person_relation
+(
+    credit_id INTEGER NOT NULL REFERENCES credit_person (id),
+    PRIMARY KEY (production_id, credit_id, credit_group_id)
+) INHERITS (production_credit_unit_relation);
