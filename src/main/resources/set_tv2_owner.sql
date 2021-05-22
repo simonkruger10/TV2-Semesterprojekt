2ae14f9ca5c5e94937b83b8f2edbd7d76f5e0350
marcus <marcp20@student.sdu.dk>
2021-05-22 12:51:25 +0200

    Create script to solve "Access denied to table 'XYZ'" when querying the database

---
+++ src/main/resources/set_tv2_owner.sql
@@ -0,0 +1,23 @@
+alter table account
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table credit_group
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table credit_person
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table credit_unit
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table producer
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table production
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table production_credit_person_relation
+    owner to tv2_dbuser_f3f70b5a;
+
+alter table production_credit_unit_relation
+    owner to tv2_dbuser_f3f70b5a;
