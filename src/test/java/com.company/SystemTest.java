c88e030c975d6dad3b3922738fb9241d2784a3b3
marcus <marcp20@student.sdu.dk>
2021-05-27 20:50:19 +0200

    add class for system testing

---
+++ src/test/java/com.company/SystemTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SystemTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void navigateProductions() {
+
+    }
+
+    @After
+    public void cleanup() {
+
+    }
+}
